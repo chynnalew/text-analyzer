39deb67a1128d4e280e7de9cb827cb05ee5b8f15
chynnalew <chynnalew@yahoo.com>
2021-08-18 09:06:31 -0700

    add bootstrap.css and jquery.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 // Utility Logic
 
 function noInputtedWord(word, text) {
-  return ((text.trim().length === 0) || (word.trim().length === 0));
+  return ((text.trim().length === 0 || word.trim().length === 0));
 }
+
 
 // Business Logic
 
@@ -28,23 +29,47 @@ function numberOfOccurrencesInText(word, text) {
   let wordCount = 0;
   wordArray.forEach(function(element) {
     if (element.toLowerCase().includes(word.toLowerCase())) {
-      wordCount++;
+      wordCount++
     }
   });
   return wordCount;
 }
 
+function numberOfWordRepeats(text) {
+  let count = [];
+  let countedWords = [];
+  const wordArray = text.toLowerCase().split(" ");
+  wordArray.forEach(function(word) {
+    let num = numberOfOccurrencesInText(word, text);
+    if (!countedWords.includes(word)) {
+      countedWords.push(word);
+      count.push(num + " " + word);
+    }
+  })
+  let initialOutput = count.sort();
+  let finalOutput = initialOutput.reverse();
+  let finalOutputArray = (finalOutput[0] + " " + finalOutput[1] + " " + finalOutput[2]);
+  let strings = finalOutputArray.split(" ");
 
+  console.log(strings);
+  console.log(finalOutputArray);
+  let mostCommonWord = strings[1] + " : " + strings[0]
+  let secondMostCommon = strings[3] + " : " + strings[2]
+  let thirdMostCommon = strings[5] + " : " + strings[4]
+  let commonWordsArray = [mostCommonWord , secondMostCommon , thirdMostCommon];
+  return commonWordsArray;
+} 
 
 // UI Logic
-function boldPassage(word, text) {
+
+function boldPassage(word,text) {
   if (noInputtedWord(word, text)) {
     return "";
   }
   let htmlString = "<p>";
   let textArray = text.split(" ");
   textArray.forEach(function(element, index) {
-    if (element.toLowerCase().includes(word.toLowerCase())) {
+    if (word === element) {
       htmlString = htmlString.concat("<b>" + element + "</b>");
     } else {
       htmlString = htmlString.concat(element);
@@ -55,15 +80,26 @@ function boldPassage(word, text) {
   });
   return htmlString + "</p>";
 }
-$(document).ready(function(){
-  $("form#word-counter").submit(function(event){
+
+function arrayToList(text) {
+  let htmlString = "";
+  text.forEach(function(element) {
+    htmlString = htmlString.concat("<li>" + element + "</li>");
+  })
+  return htmlString;
+}
+
+$(document).ready(function() {
+  $("form#word-counter").submit(function(event) {
     event.preventDefault();
     const passage = $("#text-passage").val();
     const word = $("#word").val();
     const wordCount = wordCounter(passage);
     const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+    const commonWords = numberOfWordRepeats(passage);
     $("#total-count").html(wordCount);
     $("#selected-count").html(occurrencesOfWord);
-    $("#bolded-passage").html(boldPassage(word, passage));
+    $("#bolded-passage").html(boldPassage(word,passage));
+    $("#common-words").html(arrayToList(commonWords));
   });
 });
